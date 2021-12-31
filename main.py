9a0871ee586f2f5868e674dd1dcc12bf8469d88b
Redshark61 <tim.vannier@gmail.com>
2021-12-31 18:37:19 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import json
+import random
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+import numpy as np
+# import tflearn
+# import tensorflow as tf
+nltk.download('punkt')
+stemmer = LancasterStemmer()
+
+with open("./intents.json", encoding="utf-8") as file:
+    data = json.load(file)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        tokenizedWord = nltk.word_tokenize(pattern)
+        words.extend(tokenizedWord)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+labels = sorted(labels)
+
+training = []
+output = []
+
+outEmpty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    tokenizedWord = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in tokenizedWord:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    outputRow = outEmpty[:]
+    outputRow[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(outputRow)
+
+training = np.array(training)
+output = np.array(output)
